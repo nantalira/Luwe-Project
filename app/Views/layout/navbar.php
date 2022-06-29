70981e197eff075e53b77adff21c272620dd3756
rahadiankr <rahadiankristiyanto03@gmail.com>
2022-06-30 04:12:12 +0700

    add Location

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,19 +1,25 @@
 <!-- Navbar Start -->
 <header class="navbar navbar-expand-lg bd-navbar sticky-top text-bg-light">
-    <nav class="container-xxl bd-gutter flex-wrap flex-lg-nowrap justify-content-lg-around justify-content-between" aria-label="Main navigation" id="section-nav">
+    <nav class="container-xxl bd-gutter flex-wrap flex-lg-nowrap justify-content-lg-around justify-content-between"
+        aria-label="Main navigation" id="section-nav">
+        <h5 class="">Lokasi : <span class="badge bg-primary" id="lokasi"></span></h5>
         <a class="navbar-brand" href="#">
             <img src="<?= base_url() ?>/assets/img/navbar/logo.png" alt="logo" class="icon">
         </a>
         <!-- offcanvas -->
-        <button class="btn btn-primary navbar-toggler d-flex d-lg-none order-3 p-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#bdNavbar" aria-controls="bdNavbar" aria-expanded="false" aria-label="Toggle
+        <button class="btn btn-primary navbar-toggler d-flex d-lg-none order-3 p-2" type="button"
+            data-bs-toggle="offcanvas" data-bs-target="#bdNavbar" aria-controls="bdNavbar" aria-expanded="false"
+            aria-label="Toggle
                 navigation"><i class="bi bi-list"></i>
         </button>
-        <div class="offcanvas-lg offcanvas-end d-lg-flex" id="bdNavbar" aria-labelledby="bdNavbarOffcanvasLabel" data-bs-scroll="true">
+        <div class="offcanvas-lg offcanvas-end d-lg-flex" id="bdNavbar" aria-labelledby="bdNavbarOffcanvasLabel"
+            data-bs-scroll="true">
             <div class="offcanvas-header px-4 pb-0">
                 <a class="navbar-brand" href="index2.html">
                     <img src="<?= base_url() ?>/assets/img/navbar/logo.png" alt="logo" class="img-fluid">
                 </a>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#bdNavbar" aria-label="Close"></button>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#bdNavbar"
+                    aria-label="Close"></button>
             </div>
             <div class="offcanvas-body p-4 pt-0 p-lg-0">
                 <hr class="d-lg-none text-primary-50">
@@ -37,8 +43,10 @@
                     <form action="" method="POST">
                         <div class="navbar-nav d-flex d-lg-ps-3">
                             <div class="input-group bm-0">
-                                <input type="text" class="form-control" name="keyword" placeholder="Cari Makan Yuk!" aria-label="Recipient's username" aria-describedby="basic-addon2">
-                                <button class="input-group-text" name="submit" id="basic-addon2"><i class="bi bi-search"></i></button>
+                                <input type="text" class="form-control" name="keyword" placeholder="Cari Makan Yuk!"
+                                    aria-label="Recipient's username" aria-describedby="basic-addon2">
+                                <button class="input-group-text" name="submit" id="basic-addon2"><i
+                                        class="bi bi-search"></i></button>
                             </div>
                         </div>
                     </form>
--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="/assets/css/styleshet.css">
@@ -18,12 +19,46 @@
     <?= $this->renderSection('content'); ?>
     <?= $this->include('layout/footer'); ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
     </script>
     <!-- JavaScript Bundle with Popper End -->
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
-        AOS.init();
+    AOS.init();
+    </script>
+
+    <script>
+    var spinner = document.getElementById("spinner");
+    window.addEventListener("load", function() {
+        spinner.classList.add("loaded");
+    });
+    </script>
+
+    <script>
+    //get location js
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+
+    //set location name https://nominatim.openstreetmap.org/reverse?format=jsonv2
+    function showPosition(position) {
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+        var endpoint = "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=" + lat + "&lon=" + lon;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', endpoint, true);
+        xhr.onload = function() {
+            if (this.status == 200) {
+                var data = JSON.parse(this.response);
+                console.log(data);
+                document.getElementById("lokasi").innerHTML = data.address.city;
+            }
+        };
+        xhr.send();
+    }
     </script>
 </body>
 
